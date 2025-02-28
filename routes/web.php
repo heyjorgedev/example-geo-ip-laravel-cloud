c6d551437e81e3d4f33b0ad13ccece81f9e64bfc
Jorge Lapa <2780099+heyjorgedev@users.noreply.github.com>
2025-02-28 11:20:16 +0700

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
+Route::get('/', function(Request $request) {
+    $country = $request->header('CF-IPCountry');
+
     return [
-        
+        'country' => $country,
+        'repository' => 'https://github.com/heyjorgedev/example-geo-ip-laravel-cloud',
     ];
 });
